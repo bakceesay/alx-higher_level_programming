e1a3991be558b95a639e1fd32ba631c2e980075b
bakceesay <liceesay24@gmail.com>
2023-01-23 10:26:48 -0800

    Python Exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 
-def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x elements of a list that are integers.
 
-    Args:
-        my_list (list): The list to print elements from.
-        x (int): The number of elements of my_list to print.
-
-    Returns:
-        The number of elements printed.
+def safe_print_division(a, b):
     """
-    ret = 0
-    for i in range(0, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            ret += 1
-        except (ValueError, TypeError):
-            continue
-    print("")
-    return (ret)
+    divides two integers and prints the result
+    catches divide by zero exception
+    """
+    try:
+        res = a / b
+    except:
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return res
