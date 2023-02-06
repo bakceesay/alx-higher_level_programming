8695bee884f9e6271d79a78d20559906f6bb0180
bakceesay <liceesay24@gmail.com>
2023-02-06 10:01:18 -0800

    Python - Inheritance Project

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-'''
-A class MyList that inherits from list
-'''
+"""class MyList that inherits from list
+"""
 
 
 class MyList(list):
-    '''
-    MyList class
-    '''
-
+    """inherits from list"""
     def print_sorted(self):
+        """prints the list, but sorted
+        (ascending sort)
+        """
         print(sorted(self))
